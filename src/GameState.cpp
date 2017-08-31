ff24cae2046e86cd7f279113e91dc92601b5c36b
Ryan Ewing <ewingryan98@gmail.com>
2017-08-31 17:48:08 -0400

    Added GameState

---
+++ src/GameState.cpp
@@ -0,0 +1,26 @@
+#include "GameState.hpp"
+
+GameState::GameState(StateStack& stack, Context context) : State(stack, context), world(*context.window), player(*context.player) {}
+
+void GameState::draw() {
+    this->world.draw();
+}
+
+bool GameState::update(sf::Time dt) {
+    this->world.update(dt);
+
+    CommandQueue& commands = this->world.getCommandQueue();
+    this->player.handleRealtimeInput(commands);
+
+    return true;
+}
+
+bool GameState::handleEvent(const sf::Event& event) {
+    CommandQueue& commands = this->world.getCommandQueue();
+    this->player.handleEvent(event, commands);
+
+    // if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+    //     requestStackPush(States::Pause);
+
+    return true;
+}
